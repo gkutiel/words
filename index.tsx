dfff79b30eaba46c88ebdbb8aa83ef436fc1fff5
Gilad Kutiel <gkutiel@amazon.com>
2020-05-01 17:26:24 +0300

    --

---
+++ index.tsx
@@ -0,0 +1,64 @@
+/** @jsx createElement */
+import { createElement } from 'typed-html'
+import shuffle from 'shuffle-array'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const icons = shuffle([
+        'dog'
+        , 'cat'
+        , 'ambulance'
+        , 'apple-alt'
+        , 'baby'
+        , 'baby-carriage'
+        , 'band-aid'
+        , 'battery-full'
+        , 'bed'
+        , 'beer'
+        , 'bell'
+        , 'bicycle'
+        , 'birthday-cake'
+        , 'bolt'
+        , 'bomb'
+        , 'bone'
+        , 'book'
+        , 'box-open'
+        , 'bread-slice'
+        , 'briefcase'
+        , 'bug'
+        , 'building'
+        , 'bus'
+        , 'camera'
+        , 'campground'
+        , 'car'
+        , 'carrot'
+        , 'chair'
+        , 'child'
+        , 'clock'
+        , 'coffee'
+        , 'couch'
+        , 'crow'
+        , 'crown'
+        , 'cut'
+        , 'dice-six'
+        , 'egg'
+        , 'envira'
+        , 'eye'
+        , 'faucet'
+        , 'fighter-jet'
+        , 'fish'
+        , 'flag'
+        , 'frog'
+        , 'gem'
+        , 'guitar'
+        , 'hamburger'
+    ])
+
+    const tbody = document.getElementById('tbody') as HTMLElement
+
+    for (const icon of icons.slice(0, 10)) {
+        tbody.innerHTML += <tr>
+            <td><i class={`fas fa-${icon}`}></i></td>
+            <td></td>
+        </tr>
+    }
+})
